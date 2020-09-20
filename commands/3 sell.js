f77967f8b0aa042ff4f23a28c2ab68c96acf15cd
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-09-20 18:53:46 -0400

    Update 3 sell.js

--- commands/3 sell.js
+++ commands/3 sell.js
@@ -1,5 +1,26 @@
+const Discord = require('discord.js')
+const emoji = require('../functions/utils.js').emoji
+
 exports.run = async (client, message, args) => {
-	message.channel.send( "Sell" )
+	if (args.length>0) {
+		let item = args.join(" ")
+		if (new RegExp("life potion").test(item)) {
+			let quantity = item.split("life potion").join("").trim()
+			if(/^\d+$/.test(quantity) || quantity=="") {
+				quantity = quantity ? quantity : 1
+				message.channel.send( ""+quantity+" "+emoji(client,"lifepotion")+" `life potion` successfully sold for "+(20*quantity)+" coins" )
+			} else {
+				message.channel.send( ""+message.author.toString()+", what are you doing??\n"
+					+"Correct usage: `"+CONFIG_PREFIX+"sell [item] [amount]`" )
+			}
+			// TODO: case when quantity is "all"
+		} else {
+			message.channel.send( ""+message.author.toString()+", what are you trying to sell?? check its name again\n"
+				+"Correct usage: `"+CONFIG_PREFIX+"sell [item]`" )
+		}
+	} else {
+		message.channel.send( ""+message.author.toString()+", the correct usage of this command is `"+CONFIG_PREFIX+"sell [item]`" )
+	}
 }
 
 exports.help = {
