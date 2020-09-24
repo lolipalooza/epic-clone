ab6a57f1f0c508a9461b3e90de176ff4519cde0a
lolipalooza <mumifag_lalolanda@hotmail.com>
2020-09-24 17:43:33 -0400

    Update area11 time travel.js

--- commands/area11 time travel.js
+++ commands/area11 time travel.js
@@ -9,14 +9,13 @@ exports.help = {
 	name: "time travel",
 	title: "Time Travel",
 	description: client => {
-		return "Comando para probar weas y debuggear el bot qliao."
-				+"\n\nMira que bonitos mis emojis "
-				+emoji(client,"epiccoin")+" "+emoji(client,"edgysword")+" "+emoji(client,"ancientdragon")
-				+", los saqué de 3 servers diferentes. Inteligente no? "+emoji(client,"edgysword")
+		return "If you have unlocked area 11, you can come back to the beginning, you will get **XP** and **drop** boost (also a title)\n"
+			+"Everything from your profile and inventory will be reset except your horse, coins, bank and arena cookies\n"
+			+"The more time travels you do, the faster the boost will grow (+50%, +101%, +153%...)"
 	},
 	usage: "time travel",
 	aliases: [],
-	higher_tiers: "none",
+	higher_tiers: "`super time travel`: Unlocked in area 15, but requires a **TIME key** to be used, but provides an extra reward",
 	tiers: [
 		{area:11, name:"time travel"},
 		{area:15, name:"super time travel"},
